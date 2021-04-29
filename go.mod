aa86ffd56020ed43158bfb1b255c073c7dee8a8c
xlbruce <gilsonsvieira93@gmail.com>
2021-04-29 16:37:47 -0300

    Add main code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xlbruce/vivo-pdf-bruteforce
+
+go 1.16
+
+require rsc.io/pdf v0.1.1
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+//TODO publish to github reporitory
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"rsc.io/pdf"
+)
+
+func GeneratePasswords(result *string) func() string {
+	i := 0
+	return func() string {
+		if i == 999 {
+			log.Fatal("Máximo número de tentativas. Não foi possível descriptografar o arquivo. (999)\n")
+			return ""
+		}
+
+		i++
+		p := fmt.Sprintf("%03d", i)
+		*result = p
+		return p
+	}
+}
+
+func usage() string {
+  //TODO improve binary execution
+	return `go run main.go <pdf_filename>`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal(usage())
+	}
+
+	var password string
+
+	filename := os.Args[1]
+	f, err := os.Open(filename)
+	if err != nil {
+		msg := fmt.Sprintf("Erro ao abrir o arquivo: %s\n", err.Error())
+		log.Fatal(msg)
+	}
+
+	stat, _ := f.Stat()
+	_, err = pdf.NewReaderEncrypted(f, stat.Size(), GeneratePasswords(&password))
+	if err != nil {
+		msg := fmt.Sprintf("Arquivo PDF inválido: %s", err.Error())
+		log.Fatal(msg)
+	}
+
+	if password == "" {
+		fmt.Printf("O arquivo não é protegido por senha.\n")
+	} else {
+		fmt.Printf("A senha do PDF %s é: %s\n", filename, password)
+	}
+
+}
